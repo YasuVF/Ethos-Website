b9a7cd9fccbb5d7215eeecf38fb75e65ef6fc578
YasuVF <yasuthefascist@gmail.com>
2025-06-12 01:39:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -231,8 +231,8 @@ function plot3D(x, y, z) {
     type: 'scatter3d',
     mode: 'markers',
     x: [x],
-    y: [y],
-    z: [z],
+    y: [-y],
+    z: [-z],
     marker: {
       size: 8,
       color: pointColor
